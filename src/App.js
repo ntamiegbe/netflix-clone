850068159c0c128c41b9c5a81ff0919a905e02ef
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-01 23:14:23 +0100

    Commit: Creating Login Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './index.css'
 import Hero from './pages/Homepage/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 function App() {
+
+  const user = null
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route exact path='/' element={<Hero />} />
-        </Routes>
+        {!user ? (
+          <Login />
+        ) : (
+          <Routes>
+            <Route exact path='/' element={<Hero />} />
+          </Routes>
+        )}
       </Router>
-    </div>
+    </div >
   );
 }
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,18 @@ import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div className="login__background">
+        <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" className='login__logo' />
+        <button className='login__button'>Sign In</button>
+        <div className="login__gradient" />
+      </div>
+      <div className="login__body">
+        <>
+          <h1>Unlimited films, TV programmes and more</h1>
+        </>
+      </div>
+    </div>
   )
 }
 
