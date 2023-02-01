ec7de638c383857dc31b357d33a5385a2072c8c1
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-01 01:19:47 +0100

    commit

--- src/components/Rows/Row.js
+++ src/components/Rows/Row.js
@@ -1,2 +1,37 @@
+import { useEffect, useState } from 'react'
+import './row.css'
+import axios from 'axios'
 
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+    const [movies, setMovies] = useState([])
+
+    const baseUrl = 'https://image.tmdb.org/t/p/original/'
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl])
+
+    return (
+        <div className='row'>
+            <h1>{title}</h1>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                        src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        key={movie.id}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row
 export default Row
