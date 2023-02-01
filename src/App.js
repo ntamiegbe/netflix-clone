3921463f7b54759e5cb79d5cd10a00e8ba404c42
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-01 01:18:36 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './index.css'
 import Hero from './components/Hero/Hero';
+import Row from './components/Rows/Row';
+import requests from './server/requests';
 
 function App() {
   return (
     <div className="app">
       <Hero />
+      <Row
+        title='Trending Now'
+        fetchUrl={requests.fetchTrending}
+        isLargeRow
+      />
+      <Row
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row
+        title='Top Rated'
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title='Action'
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title='Comedy'
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title='Horror'
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title='Romance'
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title='Documentaries'
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   );
 }
