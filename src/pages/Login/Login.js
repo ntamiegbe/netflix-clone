6b125ea12a1458883fcea3f5739e39914797bfcc
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-02 09:39:53 +0100

    Commit: Created Login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,6 +11,15 @@ const Login = () => {
       <div className="login__body">
         <>
           <h1>Unlimited films, TV programmes and more</h1>
+          <h2>Watch anywhere. Cancel at any time.</h2>
+          <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+          <div className="login__input">
+            <form>
+              <input type="email" placeholder='Email address'/>
+              <button className='login__getStarted'>Get Started</button>
+            </form>
+          </div>
         </>
       </div>
     </div>
