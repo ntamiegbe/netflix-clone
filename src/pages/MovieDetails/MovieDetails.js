e1887909da50ffa54cfbd3fdd0ead3c94824c3dd
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-04 20:26:10 +0100

    Commit: Fetching movie genre

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -2,22 +2,27 @@ import { useParams } from 'react-router-dom'
 import './movieDetails.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Navbar from '../../components/Navbar/Navbar'
 
 const MovieDetails = () => {
 
-    const [movie, setMovie] = useState('')
+    const [movies, setMovie] = useState('')
+    const [genres, setGenres] = useState([])
 
     const { id } = useParams()
 
+    const baseImageUrl = 'https://image.tmdb.org/t/p/original/'
     const baseUrl = 'https://api.themoviedb.org/3/'
     const API_KEY = '3a7231adb9c185338dd837061b2e2175'
+
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(`${baseUrl}movie/${id}?api_key=${API_KEY}&language=en-US`)
-            setMovie(request.data.title)
-            console.log(movie);
-            // console.log(request.data.title);
+            setMovie(request.data)
+            setGenres(request.data.genres)
+            console.log(request.data);
+            // console.log(genres);
             return request
         }
         fetchData()
@@ -25,7 +30,23 @@ const MovieDetails = () => {
 
 
     return (
-        <img src={movie} alt="" />
+        <>
+            <Navbar />
+            <div className="movieDetails">
+                <h1>{movies.title}</h1>
+                <h3>{movies.tagline}</h3>
+                <p>{movies.overview}</p>
+                <img src={`${baseImageUrl}${movies.backdrop_path || movies.poster_path}`} alt="Movie Logo" className='movieDetails__image' />
+                <p>
+                    {genres.map((genre, index) => (
+                        <span key={genre.id}>
+                            {genre.name}
+                            {index < genres.length - 1 ? ', ' : ''}
+                        </span>
+                    ))}
+                </p>
+            </div>
+        </>
     )
 }
 
